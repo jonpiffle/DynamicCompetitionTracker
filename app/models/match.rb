0a321212c6d4aa0065feabcf7c0cacd82b27f4d7
Stephanie Hippo <stephhippo@ubuntu.(none)>
2013-04-27 18:57:50 -0400

    "Added set winner"

--- app/models/match.rb
+++ app/models/match.rb
@@ -5,4 +5,16 @@ class Match < ActiveRecord::Base
   has_one :league, :through => :hangout
   belongs_to :loser, :class_name => "Team"
   belongs_to :winner, :class_name => "Team"
+
+  def set_winner
+  	if game_sets.count > (league.sets_per_match/2)
+  		winner = Hash[*game_sets.group_by(&:winner).map {|k,v| [k, v.length]}.flatten!].max_by{|k,v| v}.first
+  		loser = Hash[*game_sets.group_by(&:loser).map {|k,v| [k, v.length]}.flatten!].max_by{|k,v| v}.first
+		self.update_attributes(:winner_id => winner.id, :loser_id => loser.id)
+		true
+	else
+		false
+  	end
+  end
+
  end
