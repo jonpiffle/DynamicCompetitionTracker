15d6f4c15e81cbc7322f4b1dac1002d6046c4930
Jon Pfeil <piffle21@gmail.com>
2013-04-30 22:21:30 -0400

    fixsessionsmigrations

--- db/migrate/20130416024901_change_score_value_to_double.rb
+++ db/migrate/20130416024901_change_score_value_to_double.rb
@@ -1,6 +1,11 @@
 class ChangeScoreValueToDouble < ActiveRecord::Migration
   def up
-  	change_column :scores, :score_value, :float
+#  	change_column :scores, :score_value, :float
+    connection.execute(%q{
+        alter table scores
+        alter column score_value
+        type float using score_value::float
+    })
   end
 
   def down
